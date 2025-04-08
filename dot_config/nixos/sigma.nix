548ee9adb88de0a63ebd11e3abede000c75bba02
Phonkd <enst18.12@gmail.com>
2025-04-08 21:09:19 +0200

    idk bruh

--- dot_config/nixos/sigma.nix
+++ dot_config/nixos/sigma.nix
@@ -1,4 +1,22 @@
 { config, pkgs, lib, ... }:
 {
-  boot.kernelPackages = pkgs.linuxPackages_latest; # needed cuz new gpu driver not in current kernel
+  #boot.kernelPackages = pkgs.linuxPackages_zen; # needed cuz new gpu driver not in current kernel
+  boot.kernelPackages = pkgs.linuxPackages_testing;
+  boot = {
+    kernelParams = [ "split_lock_detect=off" "amdgpu.ppfeaturemask=0xfffd3fff" ];
+  };
+  hardware.graphics = {
+  enable = true;
+  enable32Bit = true;
+  };
+  #hardware.amdgpu.amdvlk.enable = true;
+  #services.desktopManager.plasma6.enable = true;
+  networking.nameservers = lib.mkForce [ "192.168.1.3" ];
+  system.stateVersion = lib.mkForce "25.05";
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  services.distccd.zeroconf = true;
+  environment.systemPackages = [
+    pkgs.librespot
+  ];
+
 }
